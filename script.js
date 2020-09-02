6be018b789e2681261807ab15ef5c5331f6a6e3c
Charlotte Ng <charlotte@Charlottes-MacBook-Pro.local>
2020-09-02 18:39:09 +0800

    retrieved specific data from api end-point

---
+++ script.js
@@ -0,0 +1,38 @@
+$.getJSON(
+    "https://www.googleapis.com/books/v1/volumes?q=search+terms",
+    function(data){
+        console.log(data);
+
+        var thumbnail = data.items[0].volumeInfo.imageLinks.thumbnail;
+        console.log(thumbnail);
+        
+        var title = data.items[0].volumeInfo.title;
+        console.log(title);
+
+        var author = data.items[0].volumeInfo.authors[0];
+        console.log(author);
+
+        var category = data.items[0].volumeInfo.categories[0];
+        console.log(category);
+
+        var country = data.items[0].saleInfo.country;
+        console.log(country);
+
+        var price = data.items[0].saleInfo.saleability;
+        console.log(price);
+
+        var format = data.items[0].volumeInfo.printType;
+        console.log(format);
+
+        var description = data.items[0].volumeInfo.description;
+        console.log(description);
+
+        $(".thumbnail").attr("src", thumbnail);
+        $(".author").append(author);
+        $(".category").append(category);
+        $(".country").append(country);
+        $(".price").append(price);
+        $(".format").append(format);
+        $(".description").append(description);
+    
+    })

df598dc1d03dc0cbaeb6ad67284590dfd7414f42
Charlotte Ng <charlotte@Charlottes-MacBook-Pro.local>
2020-09-07 19:59:10 +0800

    added image to search results

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-$.getJSON(
-    "https://www.googleapis.com/books/v1/volumes?q=search+terms", // load the api
-    function(data){
+// $.getJSON(
+//     "https://www.googleapis.com/books/v1/volumes?q=search+terms", // load the api
+//     function(data){
 
-        // retrieve specific data
+//         // retrieve specific data
 
-        var thumbnail = data.items[0].volumeInfo.imageLinks.thumbnail;
+//         var thumbnail = data.items[0].volumeInfo.imageLinks.thumbnail;
+//         console.log(thumbnail);
         
-        var title = data.items[0].volumeInfo.title;
-        console.log(title);
+//         var title = data.items[0].volumeInfo.title;
+//         console.log(title);
 
-        var author = data.items[0].volumeInfo.authors[0];
-        console.log(author);
+//         var author = data.items[0].volumeInfo.authors[0];
+//         console.log(author);
 
-        var category = data.items[0].volumeInfo.categories[0];
-        console.log(category);
+//         var category = data.items[0].volumeInfo.categories[0];
+//         console.log(category);
 
-        var country = data.items[0].saleInfo.country;
-        console.log(country);
+//         var country = data.items[0].saleInfo.country;
+//         console.log(country);
 
-        var price = data.items[0].saleInfo.saleability;
-        console.log(price);
+//         var price = data.items[0].saleInfo.saleability;
+//         console.log(price);
 
-        var format = data.items[0].volumeInfo.printType;
-        console.log(format);
+//         var format = data.items[0].volumeInfo.printType;
+//         console.log(format);
 
-        var description = data.items[0].volumeInfo.description;
-        console.log(description);
+//         var description = data.items[0].volumeInfo.description;
+//         console.log(description);
 
-        // display retrieved data
-        
-        $("#thumbnail").attr("src", thumbnail);
-        $("#title").append(title);
-        $("#author").append(author);
-        $("#category").append(category);
-        $("#country").append(country);
-        $("#price").append(price);
-        $("#format").append(format);
-        $("#description").append(description);
+//         // display retrieved data
 
-    });
+//         $("#thumbnail").attr("src", thumbnail);
+//         $("#title").append(title);
+//         $("#author").append(author);
+//         $("#category").append(category);
+//         $("#country").append(country);
+//         $("#price").append(price);
+//         $("#format").append(format);
+//         $("#description").append(description);
+
+//     });
 
     // create a search/submit function for the form #searchBar
     $('#searchBar').submit(function(e){
@@ -48,7 +49,7 @@ $.getJSON(
         var queryReplaced = query.replace(/ /g, '%20'); // replace any spaces with '%20'
 
         let result = ''
-    
+
         // create a valid api url with the above variables + access key
         var url = 'https://www.googleapis.com/books/v1/volumes?q=' + queryReplaced + '&key=AIzaSyBBScIhEnsywXl2UrOg90Nd4DEaDRHSdzw'
 
@@ -59,11 +60,14 @@ $.getJSON(
             data.items.forEach(res => { // get a response
                 // backtick to allow html format
                 result = `
+                    <img src=${res.volumeInfo.imageLinks.thumbnail}></img>
                     <h1>${res.volumeInfo.title}</h1>
                     <p>${res.volumeInfo.authors}</p>
                     <p>${res.searchInfo.textSnippet}</p>
                 `
-                console.log(result);
+
+                // error caught: "TypeError: Cannot read property 'textSnippet' of undefined"
+
                 $('#searchResults').append(result); // display retrieved data
 
             });

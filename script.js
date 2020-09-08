f052a4eaf4bf2e049f89f2ecb3d748925cb91496
Charlotte Ng <charlotte@Charlottes-MacBook-Pro.local>
2020-09-08 11:20:59 +0800

    solved "cannot read property of undefined"

--- script.js
+++ script.js
@@ -50,23 +50,26 @@
 
         let result = ''
 
-        // create a valid api url with the above variables + access key
-        var url = 'https://www.googleapis.com/books/v1/volumes?q=' + queryReplaced + '&key=AIzaSyBBScIhEnsywXl2UrOg90Nd4DEaDRHSdzw'
+        // create a valid api url with the above variables + pagination and access key
+        var url = 'https://www.googleapis.com/books/v1/volumes?q=' + queryReplaced + '&maxResults=40&key=AIzaSyBBScIhEnsywXl2UrOg90Nd4DEaDRHSdzw'
 
         $.get(url,function(data){ //retrieve data from the api
             console.log(data);
             $('#searchResults').html('') // clear data when there is new user input (query)
 
             data.items.forEach(res => { // get a response
-                // backtick to allow html format
-                result = `
-                    <img src=${res.volumeInfo.imageLinks.thumbnail}></img>
-                    <h1>${res.volumeInfo.title}</h1>
-                    <p>${res.volumeInfo.authors}</p>
-                    <p>${res.searchInfo.textSnippet}</p>
-                `
 
-                // error caught: "TypeError: Cannot read property of undefined"
+                result = ''
+                if (res.volumeInfo.imageLinks != null) {
+                    result += `<img src=${res.volumeInfo.imageLinks.thumbnail}></img>`;
+                } else {
+                    result += `<img src="images/default-thumbnail.png" alt="Default thumbnail image by Booksify" width="128px" height="183px"></img>`; // show default thumbnail if api's thumbnail does not exist
+                }
+
+                result += `<h1>${res.volumeInfo.title}</h1><p>${res.volumeInfo.authors}</p>`;
+                if (res.searchInfo != null) {
+                    result += `<p>${res.searchInfo.textSnippet}</p>`;
+                }
 
                 $('#searchResults').append(result); // display retrieved data
 
